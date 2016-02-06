9af6330f6a721297259ae9fc0676d201473748db
Matt Baer <matt@flashstuf.com>
2016-02-06 18:23:49 -0500

    Move meta props. to top level of response envelope
    IMPORTANT: This is a breaking change for any clients that use the
    meta.code or meta.error_msg properties. Clients should now use code
    and error_msg at the top of the response object.

--- response.go
+++ response.go
@@ -9,15 +9,10 @@ import (
 type (
 	// envelope contains metadata and optional data for a response object.
 	envelope struct {
-		MetaData metadata    `json:"meta"`
-		Data     interface{} `json:"data,omitempty"`
-	}
-
-	// metadata contains more information about the response
-	metadata struct {
-		Code         int    `json:"code"`
-		ErrorType    string `json:"error_type,omitempty"`
-		ErrorMessage string `json:"error_msg,omitempty"`
+		Code         int         `json:"code"`
+		ErrorType    string      `json:"error_type,omitempty"`
+		ErrorMessage string      `json:"error_msg,omitempty"`
+		Data         interface{} `json:"data,omitempty"`
 	}
 )
 
@@ -43,9 +38,7 @@ func renderString(w http.ResponseWriter, status int, msg string) error {
 
 func WriteSuccess(w http.ResponseWriter, data interface{}, status int) error {
 	env := &envelope{
-		MetaData: metadata{
-			Code: status,
-		},
+		Code: status,
 		Data: data,
 	}
 	return renderJSON(w, env, status)
@@ -53,10 +46,8 @@ func WriteSuccess(w http.ResponseWriter, data interface{}, status int) error {
 
 func WriteError(w http.ResponseWriter, e HTTPError) error {
 	env := &envelope{
-		MetaData: metadata{
-			Code:         e.Status,
-			ErrorMessage: e.Message,
-		},
+		Code:         e.Status,
+		ErrorMessage: e.Message,
 	}
 	return renderJSON(w, env, e.Status)
 }
